8cbdf6049b0664c4022c1cf55b0cac9aa33016cc
andrey_dergunov <andrei_dergunov@epam.com>
2024-11-25 19:00:28 +0000

    some http handler refinements

--- http_handler/http_handler.go
+++ http_handler/http_handler.go
@@ -9,39 +9,87 @@ import (
 
 type URLData = url_data.URLData
 
+// helpers
+func tokenizePath(path string) []string {
+	path = strings.Trim(path, "/") // trim leading and trailing /s
+	return strings.Split(path, "/")
+}
+
+// register new url
 func handlePOST(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/shorten", "/shorten/":
-		//TODO: store new record in the db
-
-		w.WriteHeader(http.StatusCreated)
+		// TODO: store new record in the db
+		// TODO: return http.StatusBadRequest in case of validation errors
+		w.WriteHeader(http.StatusCreated) //201
 		fmt.Fprintf(w, "Added new url to db\n")
 	default:
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s not found\n", r.URL.Path)
 	}
 }
 
+// obtain registered url
 func handleGET(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	path = strings.Trim(path, "/")
-	tokens := strings.Split(path, "/")
-	if len(tokens) == 2 {
+	tokens := tokenizePath(r.URL.Path)
+	switch len(tokens) {
+	case 2:
 		short_url := tokens[1]
 		// TODO: retrieve short url from db
-		w.WriteHeader(http.StatusOK)
+		// TODO: return 404 if short url not found
+		w.WriteHeader(http.StatusOK) //200
 		fmt.Fprintf(w, "Retrieved short url from db %s\n", short_url)
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "Method not allowed for this path\n")
+	case 3:
+		if tokens[2] == "stats" {
+			handleStats(tokens[1], w, r) // stats
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "%s not found\n", r.URL.Path)
+		}
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s not found\n", r.URL.Path)
 	}
 }
 
+// update registered url
 func handlePUT(w http.ResponseWriter, r *http.Request) {
-
+	tokens := tokenizePath(r.URL.Path)
+	switch len(tokens) {
+	case 2:
+		short_url := tokens[1]
+		// TODO: update short url in db
+		// TODO: return 404 if short url not found
+		// TODO: return 400 bad request in case of validation errors
+		w.WriteHeader(http.StatusOK) //200
+		fmt.Fprintf(w, "Updated short url %s\n", short_url)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s not found\n", r.URL.Path)
+	}
 }
 
+// remove registered url
 func handleDELETE(w http.ResponseWriter, r *http.Request) {
+	tokens := tokenizePath(r.URL.Path)
+	switch len(tokens) {
+	case 2:
+		short_url := tokens[1]
+		// TODO: delete short url from db
+		// TODO: return 404 if short url not found
+		w.WriteHeader(http.StatusNoContent) //204
+		fmt.Fprintf(w, "Deleted short url %s\n", short_url)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s not found\n", r.URL.Path)
+	}
+}
 
+// get statistics
+func handleStats(short_url string, w http.ResponseWriter, r *http.Request) {
+	//TODO: retrieve url from db and return its info
+	w.WriteHeader(http.StatusOK) //200
+	fmt.Fprintf(w, "Stats for url %s\n", short_url)
 }
 
 func shorten(w http.ResponseWriter, r *http.Request) {

546cc98988fdc8af840a0c59b0e2c4b8e5e48128
andrey_dergunov <andrei_dergunov@epam.com>
2024-11-30 20:41:30 +0000

    handle list in frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,6 +5,23 @@ const listBtn = document.getElementById("listBtn");
 const urlInput = document.getElementById("urlInput");
 const responseMsg = document.getElementById("responseMsg");
 
+async function genericRequest(url, method, body = null) {
+    const options = {
+        method: method,
+        headers: {"Content-Type": "application/json"},
+    };
+    if (body) {
+        options.body = body;
+    }
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(await response.text());
+    }
+    const data = await response.json();
+    console.log(`Response ${response.status} ${response.statusText}\n` + JSON.stringify(data));
+    return data
+}
+
 // handle save button
 saveBtn.addEventListener("click", async() => {
     const url = urlInput.value;
@@ -12,31 +29,19 @@ saveBtn.addEventListener("click", async() => {
         alert("Please enter a valid URL");
         return;
     }
-
     try {
-         // check if entered url is valid
+        // check if entered url is valid
         try {
             new URL(url);
         } catch(error) {
-            throw error
+            throw error;
         }
-        const response = await fetch(`${backUrl}`, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({url})
-        });
-        if (response.ok) {
-            // if 200..299
-            const data = await response.json();
-            console.log(`Response ${response.status} ${response.statusText}\n` + JSON.stringify(data));
-            responseMsg.innerText = `Saved as ${data.shortCode}`
-            urlInput.value = '' // clear input field
-        } else {
-            throw new Error(await response.text());
-        }
-    } catch(error) {
-        console.error("POST error: ", error);
+        const data = await genericRequest(`${backUrl}`, "POST", JSON.stringify({url}));
+        responseMsg.innerText = `Saved as ${data.shortCode}`
+        urlInput.value = '' // clear input field
+    }catch(error){
         responseMsg.innerText = `${error}`;
+        console.error("Error: ", error);
     }
 });
 
@@ -48,24 +53,34 @@ searchBtn.addEventListener("click", async() => {
         return;
     }
     try {
-        const response = await fetch(`${backUrl}/${key}`);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(`Response ${response.status} ${response.statusText}\n` + JSON.stringify(data));
-            // redirect to url
-            if (data.url) {
-                // open in a new window
-                window.open(data.url, '_blank');
-                responseMsg.innerText = `${data.url} opened in a new window`;
-            } else {
-                throw new Error("Invalid response from backend");
-            }
-            urlInput.value = '' // clear input field
+        const data = await genericRequest(`${backUrl}/${key}`, "GET");
+        // redirect to url
+        if (data.url) {
+            // open in a new window
+            window.open(data.url, '_blank');
+            responseMsg.innerText = `${data.url} opened in a new window`;
         } else {
-            throw new Error(await response.text());
+            throw new Error("Invalid response from backend");
         }
-    } catch(error) {
-        console.error("GET error: ", error);
+        urlInput.value = '' // clear input field
+    }catch(error){
         responseMsg.innerText = `${error}`;
+        console.error("Error: ", error);
+    }
+});
+
+// handle list
+listBtn.addEventListener("click", async() => {
+    try {
+        const data = await genericRequest(`${backUrl}/list`, "GET");
+        let result = "List:\n";
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+            result += `${element.shortCode}: ${element.url}\n`;
+        }
+        responseMsg.innerText = result;
+    }catch(error) {
+        responseMsg.innerText = `${error}`;
+        console.error("Error: ", error);
     }
 });
